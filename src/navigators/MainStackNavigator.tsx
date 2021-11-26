a6daa25cd96478bc3461bb9f22494e273eec43b2
Davila99 <davilaeliseo453@gmail.com>
2021-11-25 18:33:52 -0600

    Creando la App Nica Source, ademas agragando componentes de screens

---
+++ src/navigators/MainStackNavigator.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const MainStackNavigator = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default MainStackNavigator
+
+const styles = StyleSheet.create({})
---
+++ src/screens/HomeNica.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const HomeNica = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default HomeNica
+
+const styles = StyleSheet.create({})
